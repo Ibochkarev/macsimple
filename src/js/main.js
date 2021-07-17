ccb24a27b3cb23024a9c596d92fcdda135dc6503
Bochkarev Ivan <ivanx86@gmail.com>
2021-07-18 03:39:24 +0600

    Новые секции

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,32 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
+window.$ = window.jQuery = require('jquery')
+
+import { Swiper, Pagination, Mousewheel, Autoplay } from 'swiper'
+Swiper.use([Pagination, Mousewheel, Autoplay])
+
+import Bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 $(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
+    const swiper = new Swiper('.reviews__slider', {
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 1,
+        spaceBetween: 10,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 480px
+            575: {
+                slidesPerView: 2,
+                spaceBetween: 60,
+            },
+            // when window width is >= 640px
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 130,
+            },
+        },
+    })
 })
