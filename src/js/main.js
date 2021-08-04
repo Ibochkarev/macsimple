f49bf58e26efb08e30d75d49620818fde0cb9ff3
Bochkarev Ivan <ivanx86@gmail.com>
2021-08-05 00:33:02 +0600

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -7,27 +7,32 @@ Swiper.use([Pagination, Mousewheel, Autoplay])
 import Bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 $(document).ready(() => {
-    const swiper = new Swiper('.reviews__slider', {
-        centeredSlides: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        slidesPerView: 1,
-        spaceBetween: 10,
-        // Responsive breakpoints
-        breakpoints: {
-            // when window width is >= 480px
-            575: {
-                slidesPerView: 2,
-                spaceBetween: 60,
-            },
-            // when window width is >= 640px
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 35,
-            },
-            1336: { slidesPerView: 3, spaceBetween: 130 },
-        },
-    })
+  const swiper = new Swiper('.reviews__slider', {
+    centeredSlides: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 480px
+      575: {
+        slidesPerView: 2,
+        spaceBetween: 60,
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
+      1336: { slidesPerView: 3, spaceBetween: 130 },
+    },
+  })
+
+  $('.faq__more-btn').click(function() {
+    $('.faq__item').removeClass('hidden');
+    $('.faq__more-btn').hide();
+  })
 })
